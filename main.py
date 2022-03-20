6f0779f383a25e87b28d0e1152d9259e171b62a1
kgicheha <55036976+kgicheha@users.noreply.github.com>
2022-03-21 00:38:12 +0300

    Add files via upload
    with updated workout breakdown segment

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import random
 
 
-# Arrays of different workouts
+# ARRAY OF WORKOUT PLANS
 
 #chest & arms
-home_chest_workout = ["Regular Push Ups", "Incline Push Ups", "Decline Push Ups", "Wide Push Up", "Narrrow Push Up" "Diamond Push Ups", "Shoulder Taps"]
+home_chest_workout = ["Regular Push Ups", "Incline Push Ups", "Decline Push Ups", "Wide Push Up", "Narrrow Push Ups" "Diamond Push Ups", "Shoulder Taps"]
 gym_chest_workout = ["Flat Bench Press", "Inclined Press", "Decline Press", "Dumbell Press", "Chest Dips"]
 
 # abs
@@ -28,14 +28,15 @@ gym_full_workout = ["Jumping Jacks", "Chest Press with Dumbbell", "Burpees", "Mo
 
 # motivational quotes
 
-quotes = ["Of course it's hard. It's supposed to be hard. If it were easy, everybody would do it. Hard is what makes it great." ,
-            "Your body can stand almost anything. It’s your mind that you have to convince", 
-            "Hustle for that muscle.", "It comes down to one simple thing: how bad do you want it?", 
-            "Making excuses burns zero calories per hour.", "Obstacles can’t stop you. Problems can’t stop you. People can’t stop you. Only you can stop you."
-            "Exercise is like telling your body “you’re gonna hate me for this, but you’ll thank me later."
-            "The pain you feel today, will be the strength you feel tomorrow."]
+quotes = ["Of course it's hard. It's supposed to be hard. If it were easy, everybody would do it. Hard is what makes it great!" ,
+            "Your body can stand almost anything. It’s your mind that you have to convince!", 
+            "Hustle for that muscle!", "It comes down to one simple thing: how bad do you want it?", 
+            "Making excuses burns zero calories per hour!", 
+            "Obstacles can’t stop you. Problems can’t stop you. People can’t stop you. Only you can stop you!",
+            "Exercise is like telling your body “you’re gonna hate me for this, but you’ll thank me later!",
+            "The pain you feel today, will be the strength you feel tomorrow!"]
 
-# User Input
+# USER INPUT
 
 user_workout_choice = int(input("What type of workout would you like today? Type '0' for Chest & Arms, '1' for Abs, '2' for Back, '3' for Legs, '4' for Full Body \n"));
 
@@ -54,26 +55,21 @@ while (user_location > 1) or (user_location < 0):
 
 workout_time = int(input("How many minutes do you plan on working out today? 30, 45, 60 \n"));
 
-
-# if workout is at home -- jump rope as warm up
-if user_location == 0:
-    print("Your warm up: **mins jumprope")
-
-# if workout is at the gym -- treadmill as warm up
-if user_location == 1:
-    print("Your warm up: **mins on the treadmill")
+while workout_time < 30:
+    print("Not adequate time to get the best results. Increase workout to 30 minutes. It'll be worth it!")
+    workout_time = int(input("How many minutes do you plan on working out today? 30, 45, 60 \n"));
 
 
+#MOTIVATIONAL QUOTE
 
-# the more workout time, more minutes on warmup, and more sets and reps
+print("Quote of the day: "+ random.choice(quotes))
 
 
-
-#print out results & motivational quotes
+#WORKOUT PLAN
 
 #at home chest & arm workout plan
-if user_workout_choice == 0 and user_location == 1:
-    print(random.choice(home_chest_workout))
+if user_workout_choice == 0 and user_location == 0:
+    print("Today's Workout Plan: " + random.choice(home_chest_workout) + ", " + random.choice(home_chest_workout) + " & " + random.choice(home_chest_workout))
     
 #gym chest & arm workout plan
 
@@ -96,3 +92,20 @@ if user_workout_choice == 0 and user_location == 1:
 #at home
 
 #gym
+
+#WORKOUT BREAKDOWN
+
+# if workout is at home -- jump rope as warm up
+# if workout is at the gym -- treadmill as warm up
+# the more workout time, more minutes on warmup, and more sets and reps
+
+if user_location == 0 and workout_time <= 45:
+    print(f"Warm up: 15 minutes jumprope. \n Sets and reps: 3 sets, 15 reps each. \n Break in between each exercise: 1 minute")
+elif user_location == 0 and workout_time > 45:
+    print("Warm up: 20 minutes jumprope. \n Sets and reps: 5 sets, 20 reps each. \n Break in between each exercise: 2 minute")
+elif user_location == 1 and workout_time <= 45:
+  print("Warm up: 15 minutes treadmill. \n Sets and reps: 3 sets, 15 reps. \n Break in between each exercise: 1 minute")
+else:
+    print("Warm up: 20 minutes treadmill. \n Sets and reps: 3 sets, 20 reps. \n Break in between each exercise: 2 minute")
+
+
